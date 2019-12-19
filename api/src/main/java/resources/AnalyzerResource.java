d827184d2801178e48a16fd917317a17817aa25a
Robert <robikoser@gmail.com>
2019-12-19 20:17:30 +0100

    Add AWS Rekognition api

--- api/src/main/java/resources/AnalyzerResource.java
+++ api/src/main/java/resources/AnalyzerResource.java
@@ -1,14 +1,59 @@
 package resources;
 
+import clients.AmazonRekognitionClient;
+import com.amazonaws.services.rekognition.model.Label;
+
 import javax.enterprise.context.ApplicationScoped;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ApplicationScoped
 @Path("/analyze")
 public class AnalyzerResource {
+
+    @Inject
+    private AmazonRekognitionClient amazonRekognitionClient;
+
     @GET
     public Integer getCommentsList() {
        return 42;
+    }
+
+    @POST
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response uploadImage(InputStream uploadedInputStream) {
+
+        StringBuilder sb = new StringBuilder();
+
+        byte[] bytes = new byte[0];
+        try (uploadedInputStream) {
+            bytes = uploadedInputStream.readAllBytes();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Integer numberOfFaces = amazonRekognitionClient.countFaces(bytes);
+        sb.append("Number of faces " + numberOfFaces + "\n");
+
+        List<String> detectedCelebrities = amazonRekognitionClient.checkForCelebrities(bytes);
+
+        if (!detectedCelebrities.isEmpty()) {
+            sb.append(detectedCelebrities.stream().collect(Collectors.joining(",\n")));
+        }
+
+        List<Label> labels = amazonRekognitionClient.sceneDetection(bytes);
+
+        for (Label label : labels) {
+            sb.append("\nLabel: " + label.getName() + "---> Confidence: " + label.getConfidence().toString());
+        }
+
+
+        return Response.status(Response.Status.CREATED).entity(sb.toString()).build();
     }
 }
---
+++ services/src/main/java/clients/AmazonRekognitionClient.java
@@ -0,0 +1,89 @@
+package clients;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.rekognition.AmazonRekognition;
+import com.amazonaws.services.rekognition.AmazonRekognitionClientBuilder;
+import com.amazonaws.services.rekognition.model.*;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import java.nio.ByteBuffer;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ApplicationScoped
+public class AmazonRekognitionClient {
+
+//    @Inject
+//    private AppProperties appProperties;
+
+    private AmazonRekognition rekognitionClient;
+
+    @PostConstruct
+    private void init() {
+
+        AWSCredentials credentials;
+        try {
+            credentials = new BasicAWSCredentials(
+                   "AKIAYNGGH2MZDZUTZ377",
+                    "PjX1HWiyY57/qo8lJyPS8S24rzlZFANZfegKeSdc");
+        } catch (Exception e) {
+            throw new AmazonClientException("Cannot initialise the credentials.", e);
+        }
+
+        rekognitionClient = AmazonRekognitionClientBuilder
+                .standard()
+                .withRegion(Regions.EU_WEST_1)
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .build();
+
+    }
+
+    public Integer countFaces(byte[] imageBytes) {
+
+        Image image = new Image().withBytes(ByteBuffer.wrap(imageBytes));
+
+        DetectFacesRequest detectFacesRequest =
+                new DetectFacesRequest().withImage(image);
+
+        DetectFacesResult detectFacesResult = rekognitionClient.detectFaces(detectFacesRequest);
+
+        return detectFacesResult.getFaceDetails().size();
+
+    }
+
+    public List<String> checkForCelebrities(byte[] imageBytes) {
+
+        Image image = new Image().withBytes(ByteBuffer.wrap(imageBytes));
+
+        RecognizeCelebritiesRequest recognizeCelebritiesRequest =
+                new RecognizeCelebritiesRequest().withImage(image);
+
+
+        RecognizeCelebritiesResult recognizeCelebritiesResult =
+                rekognitionClient.recognizeCelebrities(recognizeCelebritiesRequest);
+
+        return recognizeCelebritiesResult.getCelebrityFaces()
+                .stream()
+                .map(celebrity -> celebrity.getName())
+                .collect(Collectors.toList());
+
+    }
+
+    public List<Label> sceneDetection(byte[] imageBytes) {
+        Image image = new Image().withBytes(ByteBuffer.wrap(imageBytes));
+
+        DetectLabelsRequest request = new DetectLabelsRequest()
+                .withImage(image)
+                .withMaxLabels(10).withMinConfidence(75F);
+
+        DetectLabelsResult result = rekognitionClient.detectLabels(request);
+
+        return result.getLabels();
+    }
+
+}
